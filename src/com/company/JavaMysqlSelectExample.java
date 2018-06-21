4c083190e10e8129443b270030ebc7aeb079cba1
Jan de Boer <jandeboer@gmail.com>
2018-06-21 16:00:39 +0200

    Added functionality

--- src/com/company/JavaMysqlSelectExample.java
+++ src/com/company/JavaMysqlSelectExample.java
@@ -9,8 +9,7 @@ import java.sql.*;
  *
  * Created by Alvin Alexander, http://alvinalexander.com
  */
-public class JavaMysqlSelectExample
-{
+public class JavaMysqlSelectExample {
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://192.168.1.10/feedback";
@@ -20,19 +19,42 @@ public class JavaMysqlSelectExample
     static final String USER = "sqluser";
     static final String PASS = "sqluserpw";
 
-    public static void main(String[] args)
-    {
-        // Connection conn = null;
-        Statement  stmt = null;
-        try
-        {
+    private static void writeResultSet(ResultSet resultSet) throws SQLException {
+        // ResultSet is initially before the first data set
+        while (resultSet.next()) {
+            // It is possible to get the columns via name
+            // also possible to get the columns via the column number
+            // which starts at 1
+            // e.g. resultSet.getSTring(2);
+            String user = resultSet.getString("myuser");
+            String website = resultSet.getString("webpage");
+            String summary = resultSet.getString("summary");
+            Date date = resultSet.getDate("datum");
+            String comment = resultSet.getString("comments");
+            System.out.println("User: " + user);
+            System.out.println("Website: " + website);
+            System.out.println("summary: " + summary);
+            System.out.println("Date: " + date);
+            System.out.println("Comment: " + comment);
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        Connection connect = null;
+        Statement statement = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
             //STEP 2: Register JDBC driver
             Class.forName(JDBC_DRIVER);
 
             //STEP 3: Open a connection
             System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
+            System.out.println("Select from Users...");
             // our SQL SELECT query.
             // if you only need a few columns, specify them by name instead of using "*"
             String query = "SELECT * FROM users";
@@ -44,8 +66,7 @@ public class JavaMysqlSelectExample
             ResultSet rs = st.executeQuery(query);
 
             // iterate through the java resultset
-            while (rs.next())
-            {
+            while (rs.next()) {
                 int id = rs.getInt("id");
                 String firstName = rs.getString("first_name");
                 String lastName = rs.getString("last_name");
@@ -57,11 +78,19 @@ public class JavaMysqlSelectExample
                 System.out.format("%s, %s, %s, %s, %s, %s\n", id, firstName, lastName, dateCreated, isAdmin, numPoints);
             }
             st.close();
-        }
-        catch (Exception e)
-        {
+
+            // Statements allow to issue SQL queries to the database
+            st = conn.createStatement();
+            // Result set get the result of the SQL query
+            System.out.println("Select from comments...");
+            resultSet = st.executeQuery("select * from feedback.comments");
+            writeResultSet(resultSet);
+
+        } catch (Exception e) {
             System.err.println("Got an exception! ");
             System.err.println(e.getMessage());
         }
     }
+
+
 }
